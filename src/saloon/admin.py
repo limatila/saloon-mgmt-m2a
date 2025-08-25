5012b25dd57ef613e5dfb16a70b84750cf0fbbcf
Átila/m2a <atilalimade@gmail.com>
2025-08-25 15:20:07 -0300

    feat: registrando modelos concretos no admin

--- src/saloon/admin.py
+++ src/saloon/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from saloon.models import *
 
 # Register your models here.
+@admin.register(Cliente)
+class ClienteAdmin(admin.ModelAdmin):
+    pass
+@admin.register(Trabalhador)
+class TrabalhadorAdmin(admin.ModelAdmin):
+    list_filter = ""
+@admin.register(Servico)
+class ServicoAdmin(admin.ModelAdmin):
+    pass
+@admin.register(Agendamento)
+class AgendamentoAdmin(admin.ModelAdmin):
+    pass

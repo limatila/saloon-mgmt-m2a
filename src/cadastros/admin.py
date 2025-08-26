7c846e014d47fe71d9f56d2124b7efbf4b42267a
Átila/m2a <atilalimade@gmail.com>
2025-08-26 16:45:25 -0300

    feat: adicionando separações de fields em admin por categoria

--- src/cadastros/admin.py
+++ src/cadastros/admin.py
@@ -1,20 +1,39 @@
 from django.contrib import admin
 
+from core.admin import BaseAdmin, PersonAdmin
+
 from cadastros.models import (
     Cliente,
     Trabalhador,
     Servico
 )
-from core.admin import PersonAdmin
+
 
 @admin.register(Cliente)
 class ClienteAdmin(PersonAdmin):
     pass
 
+
 @admin.register(Trabalhador)
 class TrabalhadorAdmin(PersonAdmin):
     pass
 
+
 @admin.register(Servico)
-class ServicoAdmin(admin.ModelAdmin):
-    pass
+class ServicoAdmin(BaseAdmin):
+    search_fields = "nome",
+    search_help_text = "Nome..."
+
+    def get_fieldsets(self, request, obj = ...):
+        base_fieldsets = list(super().get_fieldsets(request, obj))
+        
+        new_fieldsets = [
+            (
+                "Basic",
+                {
+                    'fields': ('nome', 'preco')
+                }
+            ),
+        ]
+        
+        return base_fieldsets + new_fieldsets
--- src/core/admin.py
+++ src/core/admin.py
@@ -1,8 +1,40 @@
 from django.contrib import admin
 
-class PersonAdmin(admin.ModelAdmin):
+class BaseAdmin(admin.ModelAdmin):
+    readonly_fields = ['data_criado', 'data_modificado']
+    fieldsets = [
+        (
+            'Core',
+            {
+                'fields': ('data_criado', 'data_modificado')
+            }
+        )
+    ]
+
+class PersonAdmin(BaseAdmin):
     search_fields = "nome", "cpf", "telefone", "endereco"
     search_help_text = "Nome, cpf, telefone ou endereço..."
+    
+    def get_fieldsets(self, request, obj = ...):
+        base_fieldsets = list(super().get_fieldsets(request, obj))
+        
+        new_fieldsets = [
+            (
+                "Basic",
+                {
+                    'fields': ('nome', 'cpf')
+                }
+            ),
+            (
+                "Additional",
+                {
+                    'classes': ('collapse', ),
+                    'fields': ('telefone', )
+                }
+            )
+        ]
+        
+        return base_fieldsets + new_fieldsets
 
 class DateHierarchyAdmin(admin.ModelAdmin):
     date_hierarchy = "data_criado"

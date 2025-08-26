c96e38f1ee9cc1621e2f3480d28589c147edfe39
Átila/m2a <atilalimade@gmail.com>
2025-08-26 08:54:06 -0300

    feat: incluindo urls ao project principal

--- src/project/urls.py
+++ src/project/urls.py
@@ -14,9 +14,24 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('core/', include('core.urls')),
+    path('cadastros/', include('cadastros.urls')),
+    path('servicos/', include('servicos.urls'))
 ]
+
+from django.conf.urls.static import static
+
+#! statics, remove for production
+urlpatterns.extend(
+    #So the server can see into the static files, in DEBUG mode.
+    static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+)
+urlpatterns.extend(
+    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+)

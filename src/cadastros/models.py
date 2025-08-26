07b5ec10f20ad1c7f90d6ef3c248230eab6d4373
Átila/m2a <atilalimade@gmail.com>
2025-08-26 13:42:21 -0300

    feat: tornando único os nomes de serviços

--- src/cadastros/models.py
+++ src/cadastros/models.py
@@ -25,12 +25,13 @@ class Servico(Base):
         verbose_name="Nome do serviço",
         max_length=50,
         null=False,
-        blank=False
+        blank=False,
+        unique=True
     )
     preco = models.DecimalField(
         verbose_name="Preço",
         max_digits=12,
-        decimal_places=2,
+        decimal_places=2
     )
 
     def __str__(self):

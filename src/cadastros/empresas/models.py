656230683dac00e7c39543a618cf69f2cd7819cd
Átila/m2a <atilalimade@gmail.com>
2025-08-27 15:11:34 -0300

    feat: adicionando modelo de empresa

--- src/cadastros/empresas/models.py
+++ src/cadastros/empresas/models.py
@@ -1,5 +1,26 @@
 from django.db import models
+from core.bases.models import BaseModel
 
-
-class Empresa(models.Model):
-    pass
+class Empresa(BaseModel):
+    cnpj = models.CharField(
+        verbose_name="CNPJ",
+        max_length=14,
+        unique=True,
+        null=False,
+        blank=False,
+        default=None
+    )
+    nome_fantasia = models.CharField(
+        verbose_name="Nome-fantasia",
+        max_length=255,
+        null=False,
+        blank=False,
+        default=None
+    )
+    razao_social = models.CharField(
+        verbose_name="Razão Social",
+        max_length=255,
+        null=False,
+        blank=False,
+        default=None
+    )

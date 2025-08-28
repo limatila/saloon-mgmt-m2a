0d69ce7bc6b6c7200a10beebdb10731c036f9dd4
Átila/m2a <atilalimade@gmail.com>
2025-08-28 16:26:50 -0300

    feat: criando POF de listagem de clientes

--- src/cadastros/clientes/views.py
+++ src/cadastros/clientes/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+
+from cadastros.clientes.models import Cliente
 
 
+class ClientesListView(ListView):
+    model = Cliente
+    template_name = "cliente_list.html"
+    paginate_by = 20
+
+    def get_context_data(self, **kwargs):
+        contexto = super().get_context_data(**kwargs)
+        contexto['title'] = "Lista de Clientes"
+        return contexto

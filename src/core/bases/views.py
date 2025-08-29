21f9de74757ebefe5cbf0b3e983b04657f3e2bb3
Átila/m2a <atilalimade@gmail.com>
2025-08-29 17:26:42 -0300

    feat: iniciando desevolvimento da home page, POF montado

--- src/core/bases/views.py
+++ src/core/bases/views.py
@@ -1,21 +1,32 @@
 from django.core.exceptions import ImproperlyConfigured
-from django.views import View
+# from django.views import View
 from django.views.generic import TemplateView
-from django.views.generic import CreateView, ListView, UpdateView #DeleteView não recomendado, apenas inativar o registro.
+from django.views.generic import ListView#, CreateView, UpdateView #DeleteView não recomendado, apenas inativar o registro.
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-class BaseView(TemplateView):
+class BasePageView(LoginRequiredMixin, TemplateView):
     template_name = "base.html"
 
     def get_context_data(self, **kwargs):
         contexto = super().get_context_data(**kwargs)
         contexto["title"] = "Base"
-        contexto["nome"] = "atila"
+        contexto["sidebar"] = True
+        return contexto
+
+
+class HomePageView(LoginRequiredMixin, TemplateView):
+    template_name = "home.html"
+
+    def get_context_data(self, **kwargs):
+        contexto = super().get_context_data(**kwargs)
+        contexto["title"] = "Home"
+        contexto["sidebar"] = False
         return contexto
 
 
 # bases de componentes
-class BaseDynamicListView(ListView):
+class BaseDynamicListView(LoginRequiredMixin, ListView):
     """
     Uma view para iterar sobre campos de objetos.
     var 'model' deve ser definido.
@@ -30,7 +41,7 @@ class BaseDynamicListView(ListView):
         )
 
     def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
+        contexto = super().get_context_data(**kwargs)
 
         field_order = self.get_fields_display()
 
@@ -41,18 +52,19 @@ class BaseDynamicListView(ListView):
             except Exception:
                 raise ImproperlyConfigured(f"'{field}' não é um field de {self.model.__name__}")
 
-        context['field_names'] = [
+        contexto['field_names'] = [
             self.model._meta.get_field(field).verbose_name
             for field in field_order
         ]
-        context['object_dicts'] = [
+        contexto['object_dicts'] = [
             {
                 field: getattr(obj, f"get_{field}_display")()
                 if callable(getattr(obj, f"get_{field}_display", None))
                 else getattr(obj, field)
                 for field in field_order
             }
-            for obj in context['object_list']
+            for obj in contexto['object_list']
         ]
+        contexto["sidebar"] = True
 
-        return context
+        return contexto

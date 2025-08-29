f5881fe25d29405607fad1d7e24da8c2d4a3d4f0
Átila/m2a <atilalimade@gmail.com>
2025-08-29 08:56:26 -0300

    feat: finalizando view de listagem de clientes

--- src/cadastros/clientes/views.py
+++ src/cadastros/clientes/views.py
@@ -1,15 +1,12 @@
-from django.shortcuts import render
 from django.views.generic import ListView
 
 from cadastros.clientes.models import Cliente
 
-
 class ClientesListView(ListView):
     model = Cliente
-    template_name = "cliente_list.html"
-    paginate_by = 20
+    
 
-    def get_context_data(self, **kwargs):
+    def get_context_data(self, *args, **kwargs):
         contexto = super().get_context_data(**kwargs)
         contexto['title'] = "Lista de Clientes"
         return contexto

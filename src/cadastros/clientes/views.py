7daf36cd095eecc60d3aa792caf1cc12453dda4a
Átila/m2a <atilalimade@gmail.com>
2025-08-29 10:22:43 -0300

    feat: finalizando conforme implementação sobre DynamicListView

--- src/cadastros/clientes/views.py
+++ src/cadastros/clientes/views.py
@@ -1,10 +1,12 @@
-from django.views.generic import ListView
+from core.bases.views import DynamicListView
 
 from cadastros.clientes.models import Cliente
 
-class ClientesListView(ListView):
+class ClientesListView(DynamicListView):
     model = Cliente
-    
+    def get_fields_display(self):
+        return ['nome', 'cpf', 'imagem', 'telefone', 'endereco', 'data_criado']
+
 
     def get_context_data(self, *args, **kwargs):
         contexto = super().get_context_data(**kwargs)

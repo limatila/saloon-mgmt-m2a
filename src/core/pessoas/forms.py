ed1b63e7f2cc1d0ae1c895104f2375ff997dfa4b
Átila/m2a <atilalimade@gmail.com>
2025-09-08 09:51:59 -0300

    fix + style: corrigindo validação de campos com nome errado, trocando ordem de métodos priorizando importancia

--- src/core/pessoas/forms.py
+++ src/core/pessoas/forms.py
@@ -11,26 +11,26 @@ class BasePessoasForm(forms.ModelForm):
             'cpf': forms.TextInput(attrs={'class': 'form-field', 'placeholder': "XXX.XXX.XXX-xx"}),
             'telefone': forms.TextInput(attrs={'class': 'form-field', 'placeholder': "+55 (XX) xxxx.xxxx"}),
             'endereco': forms.TextInput(attrs={'class': 'form-field', 'placeholder': "nome de nome"}),
-            'imagem': forms.FileInput(attrs={'class': 'form-field', 'required': 'False'}),
+            'imagem': forms.FileInput(attrs={'class': 'form-field'}),
         }
 
-    def clean_phone_number(self):
-        number: str = self.cleaned_data.get('phone_number')
-        if number:
-            charsToRemove = [' ', '(', ')']
-            if not number.startswith('+'):
-                raise forms.ValidationError('Phone number must start with \'+\'')
-            for char in charsToRemove:
-                number = number.replace(char, '')
-        return number
-    
-    def clean_CPF(self):
-        cpf = self.cleaned_data.get('CPF')
+    def clean_cpf(self):
+        cpf = self.cleaned_data.get('cpf')
         if cpf:
             validator = CPF()
             charsToRemove = [' ', '.', '-']
             for char in charsToRemove:
                 cpf = cpf.replace(char, '')   
             if not validator.validate(cpf):
-                raise forms.ValidationError("CPF is not valid.")
+                raise forms.ValidationError("CPF não é válido")
         return cpf
+    
+    def clean_telefone(self):
+        number: str = self.cleaned_data.get('telefone')
+        if number:
+            charsToRemove = [' ', '(', ')']
+            if not number.startswith('+'):
+                raise forms.ValidationError('Número de telefone deve começar com \'+\'')
+            for char in charsToRemove:
+                number = number.replace(char, '')
+        return number

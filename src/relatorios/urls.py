5ae5d771629551254fff3193874ffa50e30f2490
Átila/m2a <atilalimade@gmail.com>
2025-09-10 08:40:13 -0300

    feat: finalizando menu de seleção de relatórios

--- src/relatorios/urls.py
+++ src/relatorios/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
-from .views import RelatorioAgendamentosMensalView
+from relatorios.views import SelecaoRelatoriosView, RelatorioAgendamentosMensalView, RelatorioClientesMensalView
 
 app_name = 'relatorios'
 
 urlpatterns = [
-    path('', RelatorioAgendamentosMensalView.as_view(), name='list'),
-    path('atividade-mensal/', RelatorioAgendamentosMensalView.as_view(), name='mensal') # Exemplo de URL: /relatorios/atividade-mensal/?ano=2023&mes=10
+    path('', SelecaoRelatoriosView.as_view(), name='list'),
+    path('atividade-mensal/', RelatorioAgendamentosMensalView.as_view(), name='atividade-mensal'), # Exemplo de URL: /relatorios/atividade-mensal/?ano=2023&mes=10
+    path('clientes-mensal/', RelatorioClientesMensalView.as_view(), name='clientes-mensal')
 ]
+
+#selecione urlpatterns que devem se motrados na listagem de relatórios
+display_nomes: dict[str, str] = {
+    #[urlpattern.name]: "nome para display"
+    'mensal': "Relatório de Atividade Mensal",
+}
--- src/relatorios/views.py
+++ src/relatorios/views.py
@@ -1,12 +1,14 @@
 from datetime import datetime
 
-from django.http import HttpResponse
 from django.views import View
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponse
+from django.urls import reverse_lazy
 
+from core.bases.views import BasePageView
 from cadastros.empresas.mixins import EscopoEmpresaQuerysetMixin
 
-from relatorios.relatorios import RelatorioAgendamentoMensal
+from .relatorios import RelatorioAgendamentoMensal
 
 
 class BaseReportView(LoginRequiredMixin, EscopoEmpresaQuerysetMixin, View):
@@ -39,6 +41,30 @@ class BaseReportView(LoginRequiredMixin, EscopoEmpresaQuerysetMixin, View):
 
 
 #* Especializados: page, relatórios
+
+class SelecaoRelatoriosView(LoginRequiredMixin, EscopoEmpresaQuerysetMixin, BasePageView):
+    template_name = 'relatorios/selecao-relatorios.html'
+
+    def get_relatorios_list(self) -> list[dict[str, str]]:
+        from relatorios.urls import urlpatterns, display_nomes
+
+        relatorios = []
+        for url in urlpatterns:
+            nome_mostrado = display_nomes.get(url.name, None)
+            if nome_mostrado:# só pega rotas nomeadas
+                relatorios.append({
+                    "nome": (nome_mostrado or url.name).replace("_", " ").title(),
+                    "url": reverse_lazy(f"relatorios:{url.name}"),
+                })
+        return relatorios
+
+    def get_context_data(self, **kwargs):
+        contexto = super().get_context_data(**kwargs)
+        contexto["title"] = "Lista de relatórios"
+        contexto["description"] = "Selecione um relatório para exibição em PDF."
+        contexto["relatorios_list"] = self.get_relatorios_list()
+        return contexto
+
 
 class RelatorioAgendamentosMensalView(BaseReportView):
     """

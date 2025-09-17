c946aaea246e1746001ea47b4cfa2d2c90465c85
Átila/m2a <atilalimade@gmail.com>
2025-09-17 13:44:50 -0300

    refactor: adicionando campos de novos fields em modelo

--- src/cadastros/empresas/admin.py
+++ src/cadastros/empresas/admin.py
@@ -4,6 +4,39 @@ from core.bases.admin import BaseAdmin
 from cadastros.empresas.models import Empresa
 
 
+@admin.register(Empresa)
+class EmpresaAdmin(BaseAdmin):
+    search_fields = "cnpj", "nome_fantasia", "razao_social"
+    search_help_text = "CNPJ, nome fantasia ou razao social..."
+
+    def get_list_display(self, request):
+        base_list = list(super().get_list_display(request))
+
+        new_list = ["cnpj", "nome_fantasia", "razao_social", "imagem"]
+
+        return new_list + base_list
+
+    def get_fieldsets(self, request, obj = ...):
+        base_fieldsets = list(super().get_fieldsets(request, obj))
+
+        new_fieldsets = [
+            (
+                "Basic",
+                {
+                    'fields': ('cnpj', 'nome_fantasia', 'razao_social', )
+                }
+            ),
+            (
+                "Additional",
+                {
+                    'fields': ('user', 'imagem', )
+                }
+            )
+        ]
+        
+        return base_fieldsets + new_fieldsets
+
+
 class BaseAssociadoEmpresaAdmin(BaseAdmin):
     raw_id_fields = "empresa", 
     
@@ -22,33 +55,6 @@ class BaseAssociadoEmpresaAdmin(BaseAdmin):
                 "Empresas",
                 {
                     'fields': ('empresa', )
-                }
-            ),
-        ]
-        
-        return base_fieldsets + new_fieldsets
-
-
-@admin.register(Empresa)
-class EmpresaAdmin(BaseAdmin):
-    search_fields = "cnpj", "nome_fantasia", "razao_social"
-    search_help_text = "CNPJ, nome fantasia ou razao social..."
-
-    def get_list_display(self, request):
-        base_list = list(super().get_list_display(request))
-
-        new_list = ["cnpj", "nome_fantasia", "razao_social"]
-
-        return new_list + base_list
-
-    def get_fieldsets(self, request, obj = ...):
-        base_fieldsets = list(super().get_fieldsets(request, obj))
-
-        new_fieldsets = [
-            (
-                "Basic",
-                {
-                    'fields': ('cnpj', 'nome_fantasia', 'razao_social')
                 }
             ),
         ]
